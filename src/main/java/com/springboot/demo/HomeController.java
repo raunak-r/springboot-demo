71d9f70214745e1c6b5a92004d570bf770b91fd8
Raunak <rritesh@deloitte.com>
2019-09-17 20:42:00 +0530

    ModelAndView object Implementation

--- src/main/java/com/springboot/demo/HomeController.java
+++ src/main/java/com/springboot/demo/HomeController.java
@@ -6,21 +6,21 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 	
 	@RequestMapping("home")
-	public String home(HttpServletRequest req){
-		HttpSession session = req.getSession();
+	public ModelAndView home(@RequestParam("name") String myName){
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("name", myName);
+		mv.setViewName("home");
 		
-		String name = req.getParameter("name");
-				
 		System.out.println("Inside Home fn in HomeController");
-		System.out.println("Hello, " + name);
+		System.out.println("Hello, " + myName);
 		
-		session.setAttribute("name", name);
-		
-		return "home";
+		return mv;
 	}
 }
