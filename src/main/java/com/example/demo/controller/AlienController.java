aa7ab6e05e7d587b436c15b55fd09d48fe1e1c8b
Raunak <raunakritesh.india@gmail.com>
2019-09-23 18:28:08 +0530

    Delete, put methods added

--- src/main/java/com/example/demo/controller/AlienController.java
+++ src/main/java/com/example/demo/controller/AlienController.java
@@ -4,8 +4,10 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,7 +31,7 @@ public class AlienController {
 	
 	
 	//***************GET One Object at a time
-//	localhost:8080/getAlien/?aid=1  | as an HTML
+//	localhost:8080/getAlien/?aid=1  || as an HTML
 	@RequestMapping("/getAlien")
 	public ModelAndView getAlien(@RequestParam int aid) {
 		ModelAndView mv = new ModelAndView("showAlien");
@@ -43,14 +45,14 @@ public class AlienController {
 		return mv;
 	}
 	
-//	localhost:8080/getAlien/1  | as an object/text
+//	localhost:8080/getAlien/1  || as an object/text
 	@RequestMapping("/getAlien/{aid}")
 	@ResponseBody
 	public String getAliens(@PathVariable("aid") int aid){
 		return repo.findById(aid).toString();
 	}
 	
-//	localhost:8080/getAlien/optional/1  | in JSON format.
+//	localhost:8080/getAlien/optional/1  || in JSON format.
 	@RequestMapping("/getAlien/optional/{aid}")
 	@ResponseBody
 	public Optional<Alien> getOptionalAliens(@PathVariable("aid") int aid){
@@ -60,14 +62,14 @@ public class AlienController {
 	
 	
 	//***************GET List of Objects
-//	localhost:8080/aliens  | Text List
+//	localhost:8080/aliens  || Text List
 	@RequestMapping("/aliens")
 	@ResponseBody
 	public String getAliens(){
 		return repo.findAll().toString();
 	}
 	
-//	localhost:8080/aliens/list  | JSON Format
+//	localhost:8080/aliens/list  || JSON Format
 	@RequestMapping("/aliens/list")
 	@ResponseBody
 	public List<Alien> getAlienList(){
@@ -77,6 +79,7 @@ public class AlienController {
 	
 	
 	//***************POST new alien
+//	from the form
 	@RequestMapping("/addAlien")
 	public String addAlien(Alien alien) {
 		repo.save(alien);
@@ -84,7 +87,28 @@ public class AlienController {
 	}
 	
 	@PostMapping("/alien")
+//	from the postman, raw or form data
 	public Alien addAlienPost(@RequestBody Alien alien) {
+		repo.save(alien);
+		return alien;
+	}
+	//***************
+	
+	
+	//***************DELETE Alien
+//	localhost:8080/alien/6
+	@DeleteMapping("/alien/{aid}")
+	public String deleteAlien(@PathVariable int aid) {
+		Alien a = repo.getOne(aid);
+		repo.delete(a);
+		
+		return "Succesfully Deleted";
+	}
+	
+	//***************PUT Alien
+//	localhost:8080/alien/6  || AID should match
+	@PutMapping("/alien")
+	public Alien saveOrUpdateAlien(@RequestBody Alien alien) {
 		repo.save(alien);
 		return alien;
 	}
